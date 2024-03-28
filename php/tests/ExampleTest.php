81cc05de6487d4158709d39557821a3ceaf10d48
Patrick Durold <patrick@durold.de>
2024-03-28 13:54:46 +0100

    Add PHP workspace and example tests

--- php/tests/ExampleTest.php
+++ php/tests/ExampleTest.php
@@ -2,6 +2,27 @@
 
 namespace Tests;
 
+use Example;
+use Exception;
+use PHPUnit\Framework\TestCase;
+
 class ExampleTest extends TestCase
 {
+    public Example $sut;
+    protected function setUp(): void
+    {
+        $this->sut = new Example();
+    }
+
+    public function testFortyTwo()
+    {
+        $this->assertEquals(42, $this->sut->iKnowFortyTwo());
+    }
+
+    public function testRaising()
+    {
+        $this->expectException(Exception::class);
+
+        throw new Exception("I'm an exception!");
+    }
 }
